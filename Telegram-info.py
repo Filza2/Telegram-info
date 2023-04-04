42d0b6b9fafa1e6064092a001dc9d2e9ce6473c0
TweakPY <69265406+Filza2@users.noreply.github.com>
2023-04-04 03:39:18 +0300

    Update Telegram-info.py

--- Telegram-info.py
+++ Telegram-info.py
@@ -1,28 +1,44 @@
+from rich.console import Console
 from requests import get
-import re
-print("""
-████████╗███████╗██╗     ███████╗    ███╗   ██╗███████╗
-╚══██╔══╝██╔════╝██║     ██╔════╝    ████╗  ██║██╔════╝
-   ██║   █████╗  ██║     █████╗      ██╔██╗ ██║█████╗  
-   ██║   ██╔══╝  ██║     ██╔══╝      ██║╚██╗██║██╔══╝  
-   ██║   ███████╗███████╗███████╗    ██║ ╚████║██║     
-   ╚═╝   ╚══════╝╚══════╝╚══════╝    ╚═╝  ╚═══╝╚═╝                                                            
+import re,os
 
-             By  @TweakPY - @vv1ck
+console=Console()
+
+def header():
+    os.system('cls' if os.name=='nt' else 'clear');console.print("""
+████████╗███████╗██╗     ███████╗              ██╗███╗   ██╗███████╗ ██████╗ 
+╚══██╔══╝██╔════╝██║     ██╔════╝              ██║████╗  ██║██╔════╝██╔═══██╗
+   ██║   █████╗  ██║     █████╗      █████╗    ██║██╔██╗ ██║█████╗  ██║   ██║
+   ██║   ██╔══╝  ██║     ██╔══╝      ╚════╝    ██║██║╚██╗██║██╔══╝  ██║   ██║
+   ██║   ███████╗███████╗███████╗              ██║██║ ╚████║██║     ╚██████╔╝
+   ╚═╝   ╚══════╝╚══════╝╚══════╝              ╚═╝╚═╝  ╚═══╝╚═╝      ╚═════╝ 
+                                                                                                                                        
+                             By  @TweakPY - @vv1ck
 """)
-user=input("[?] Enter the user : ")
-r=get(f"https://t.me/{user}")
+header()
+    
+user=input("- Enter The username : ").strip();header()
 try:
-	if r.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0:
-		print("[!] user Not Found !")
-	else:
-		try:
-			username=re.findall(f'<title>Telegram: Contact (.*?)</title>',r.text)[0]
-			name=re.findall(f'<meta property="og:title" content="(.*?)">',r.text)[0]
-			description=re.findall(f'<meta property="og:description" content="(.*?)">',r.text)[0]
-			try:
-				subscribers_count=re.findall(f'<div class="tgme_page_extra">(.*?)</div>',r.text)[0]
-				print(str(f'[+] username: [{username}]\n[+] name: [{name}]\n[+] description: [{description}]\n[+] subscribers count: [{subscribers_count}]\n------------------------'))
-			except:print(str(f'[+] username: [{username}]\n[+] name: [{name}]\n[+] description: [{description}]\n------------------------'))
-		except:print('[!] user Not valid')
-except:print("[!] Error ...")
+    r=get(f"https://t.me/{user}")
+    if r.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0:console.print("- [bold red]Error[/bold red], user Not Found !");exit()
+    else:
+        try:
+            username=re.findall(f'<title>Telegram: Contact (.*?)</title>',r.text)[0]
+            name=re.findall(f'<meta property="og:title" content="(.*?)">',r.text)[0];name=str(name).replace('\t', '')
+            description=re.findall(f'<meta property="og:description" content="(.*?)">',r.text)[0];description=str(description).replace('\t', '')
+            try:
+                subscribers_count=re.findall(f'<div class="tgme_page_extra">(.*?)</div>',r.text)[0]
+                console.print(f"""- username : [bold red]{username}[/bold red]
+- Name : [bold red]{name}[/bold red]
+- Description : [bold red]{description}[/bold red]
+- Subscribers Count : [bold red]{subscribers_count}[/bold red]
+- Type : [bold red]Channel[/bold red]""")
+            except Exception as e:
+                console.print(f"""- username : [bold red]{username}[/bold red]
+- Name : [bold red]{name}[/bold red]
+- Description : [bold red]{description}[/bold red]
+- Type : [bold red]user[/bold red]""")
+        except Exception as e:console.print("- [bold red]Error[/bold red], user Not Valid !");exit()
+		
+		
+except Exception as e:console.print('- [bold red]Error ![/bold red]');exit()
